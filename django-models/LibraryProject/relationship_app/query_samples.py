01fad426686194a5bd9f133afc08064e1a56a1cd
Obaro Awowede <akpobaroawowede@gmail.com>
2025-07-17 14:50:24 +0100

    fixed structure

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -23,8 +23,9 @@ def test_data():
 def queries():
     ## query for searching for books by a specific author
     try:
-        tolkien = Author.objects.get(name ="Tolkien")
-        books_by_tolkien = Book.objects.filter(author = tolkien)
+        author_name = "Tolkien"
+        author = Author.objects.get(name=author_name)
+        books_by_tolkien = objects.filter(author=author)
         if books_by_tolkien.exists():
             print("Below are the books:")
             for book in books_by_tolkien:
