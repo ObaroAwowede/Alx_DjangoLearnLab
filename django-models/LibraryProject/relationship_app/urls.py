40f0604c00678d7c4ee5a6612c4d13c168a0a33e
Obaro Awowede <akpobaroawowede@gmail.com>
2025-07-18 10:40:28 +0100

    individually imported list_books to pass checker

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import LibraryDetailView, list_books 
+from .views import LibraryDetailView
+from .views import list_books
 
 app_name = 'relationship_app'
 
