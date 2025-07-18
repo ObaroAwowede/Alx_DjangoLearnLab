bd8ac3a882152d3334e2ac32788c2478178db6bc
Obaro Awowede <akpobaroawowede@gmail.com>
2025-07-18 10:39:08 +0100

    modified import statement and changed view name to pass checker

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import LibraryDetail, list_books 
+from .views import LibraryDetailView, list_books 
 
 app_name = 'relationship_app'
 
 urlpatterns = [
-    path('libraries/<int:pk>/', LibraryDetail.as_view(), name='library_detail'),
+    path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
     path('books/', list_books, name='book_list'),
 ]
