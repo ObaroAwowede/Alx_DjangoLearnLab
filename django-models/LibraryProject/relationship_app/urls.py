77e41e01261a11e4d3cc19a5816065e5cb573adb
Obaro Awowede <akpobaroawowede@gmail.com>
2025-07-21 00:01:49 +0100

    fixed variable name

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     path('admin/dashboard/', views.admin_dashboard_view, name='admin_dashboard'),
     path('librarian/dashboard/', views.librarian_dashboard_view, name='librarian_dashboard'),
     path('member/dashboard/', views.member_dashboard_view, name='member_dashboard'),
-    path('books/<int:pk>/edit/', views.BookUpdateView.as_view(), name='edit_book'),
+    path('books/<int:pk>/edit/', views.BookUpdateView.as_view(), name='edit_book/'),
     path('books/<int:pk>/delete/', views.BookDeleteView.as_view(), name='delete_book'),
-    path('books/add/', views.BookCreateView.as_view(), name='add_book'),
+    path('books/add/', views.BookCreateView.as_view(), name='add_book/'),
 ]
