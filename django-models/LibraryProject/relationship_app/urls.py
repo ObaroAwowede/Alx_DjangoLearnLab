bf18dbce8fbdb22c3ec266cc9b3e58b7f5e3f665
Obaro Awowede <akpobaroawowede@gmail.com>
2025-07-20 21:19:00 +0100

    modified urls.py to pass checker

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path
-from .views import LibraryDetailView, UserRegistrationView, UserLoginView, UserLogoutView
-from .views import list_books
-
+from django.contrib.auth import views as auth_views
+from .views import LibraryDetailView, UserRegistrationView, list_books
 
 app_name = 'relationship_app'
 
 urlpatterns = [
     path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
     path('books/', list_books, name='book_list'),
-    path('register/', UserRegistrationView.as_view(), name='register'),
-    path('login/', UserLoginView.as_view(), name='login'),
-    path('logout/', UserLogoutView.as_view(), name='logout'),
+    path('register/', UserRegistrationView.as_view(), name='register'), 
+    path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logged_out.html'), name='logout'),
 ]
