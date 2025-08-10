db2d6e3931784ccb8574a748fe35052f374569b6
Obaro Awowede <akpobaroawowede@gmail.com>
2025-08-10 20:37:04 +0100

    Fixed import statements to pass checker

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, permissions, filters
+from django_filters import rest_framework
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
