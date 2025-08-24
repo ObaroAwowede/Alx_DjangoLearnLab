33d1571359aaab95e029b098c2eab48b5d19c968
Obaro Awowede <akpobaroawowede@gmail.com>
2025-08-24 12:10:57 +0100

    Done with task 2

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -5,3 +5,12 @@ class User(AbstractUser):
     bio = models.TextField()
     profile_picture = models.ImageField()
     followers = models.ManyToManyField("self", symmetrical=False)
+    following = models.ManyToManyField(
+        "self",
+        symmetrical=False,
+        related_name="followed_by",
+        blank=True
+    )
+
+    def __str__(self):
+        return self.username
